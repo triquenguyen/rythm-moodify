12d65cc096e7fd49fb329c6641d5132b5110efb3
Trique Nguyen <ngngoctridung123@gmail.com>
2023-10-28 04:08:31 -0700

    first commit

---
+++ client/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "dev-olog88c7d4x1phbb.us.auth0.com",
+      applicationID: "mGqeZDZOexm765AzzrCyU5ISI9mkQFW1",
+    },
+  ]
+};
---
+++ client/src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithAuth0 } from "convex/react-auth0";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-olog88c7d4x1phbb.us.auth0.com"
+      clientId="mGqeZDZOexm765AzzrCyU5ISI9mkQFW1"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <ConvexProviderWithAuth0 client={convex}>
+        <App />
+      </ConvexProviderWithAuth0>
+    </Auth0Provider>
+  </React.StrictMode>
+);
